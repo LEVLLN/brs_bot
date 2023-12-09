9aefba7a123bbac48d3884bde62e49a49c9bc63c
Лев Кудряшов <lev.kudryashov1994@gmail.com>
2023-12-10 01:30:11 +0500

    Commands static keywords update. Add CommandProperty

--- src/core/command.rs
+++ src/core/command.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::cmp::Reverse;
+use std::collections::HashSet;
 
 use once_cell::sync::{Lazy, OnceCell};
 use strum::IntoEnumIterator;
@@ -7,9 +8,7 @@ use strum_macros::EnumIter;
 use Command::*;
 use Token::*;
 
-use crate::core::lexer::Token;
-
-type CommandKeyWordsTokens = Vec<Vec<Token<'static>>>;
+use crate::core::lexer::{tokenize, Token};
 
 static BOT_KEY_WORDS: Lazy<Vec<Token>> =
     Lazy::new(|| vec![Word("хлеб"), Word("хлебушек"), Word("bread_bot")]);
@@ -44,117 +43,49 @@ pub struct CommandProperty<'a> {
     pub command_end_position: usize,
 }
 
-pub fn command_keywords() -> &'static HashMap<&'static Command, CommandKeyWordsTokens> {
-    static INSTANCE: OnceCell<HashMap<&Command, CommandKeyWordsTokens>> = OnceCell::new();
+fn command_keywords<'a>() -> &'static Vec<(&'a Command, Vec<Token<'a>>)> {
+    static INSTANCE: OnceCell<Vec<(&Command, Vec<Token>)>> = OnceCell::new();
     INSTANCE.get_or_init(|| {
-        let result: HashMap<&Command, CommandKeyWordsTokens> = HashMap::from([
-            (
-                &Help,
-                vec![vec![Word("help")], vec![Word("помощь")], vec![Word("хелп")]],
-            ),
-            (&Who, vec![vec![Word("кто")], vec![Word("who")]]),
-            (
-                &AnswerChance,
-                vec![
-                    vec![Word("процент"), Word("срабатывания")],
-                    vec![Word("процент")],
-                ],
-            ),
-            (&Show, vec![vec![Word("покажи")], vec![Word("show")]]),
-            (&Add, vec![vec![Word("добавь")], vec![Word("add")]]),
-            (
-                &Remember,
-                vec![
-                    vec![Word("запомни")],
-                    vec![Word("remember")],
-                    vec![Word("запомни"), Word("значение")],
-                ],
-            ),
-            (&Delete, vec![vec![Word("удали")], vec![Word("delete")]]),
-            (
-                &Check,
-                vec![
-                    vec![Word("проверь")],
-                    vec![Word("проверка")],
-                    vec![Word("check")],
-                ],
-            ),
-            (&Say, vec![vec![Word("скажи")], vec![Word("say")]]),
-            (
-                &Couple,
-                vec![
-                    vec![Word("парочка")],
-                    vec![Word("пара")],
-                    vec![Word("couple")],
-                ],
-            ),
-            (&Top, vec![vec![Word("топ")], vec![Word("top")]]),
-            (
-                &Channel,
-                vec![
-                    vec![Word("channel")],
-                    vec![Word("all")],
-                    vec![Word("канал")],
-                ],
-            ),
-            (
-                &RandomChance,
-                vec![
-                    vec![Word("вероятность")],
-                    vec![Word("шанс")],
-                    vec![Word("chance")],
-                ],
-            ),
-            (
-                &RandomChoose,
-                vec![
-                    vec![Word("выбери")],
-                    vec![Word("выбор")],
-                    vec![Word("choose")],
-                ],
-            ),
-            (
-                &GenerateNonsense,
-                vec![
-                    vec![Word("бред")],
-                    vec![Word("давай")],
-                    vec![Word("nonsense")],
-                ],
-            ),
-            (
-                &Morph,
-                vec![
-                    vec![Word("морф")],
-                    vec![Word("морфируй")],
-                    vec![Word("morph")],
-                ],
-            ),
+        let property = vec![
+            (&Help, vec!["хелп", "хлеп", "help"]),
+            (&Who, vec!["кто", "who"]),
+            (&AnswerChance, vec!["процент", "процент срабатывания"]),
+            (&Show, vec!["покажи", "show"]),
+            (&Add, vec!["добавь", "add"]),
+            (&Remember, vec!["запомни", "запомни значение", "remember"]),
+            (&Delete, vec!["удали", "delete"]),
+            (&Check, vec!["проверь", "проверка", "check"]),
+            (&Say, vec!["скажи", "say"]),
+            (&Couple, vec!["парочка", "пара", "couple"]),
+            (&Top, vec!["топ", "top"]),
+            (&Channel, vec!["канал", "channel", "all"]),
+            (&RandomChance, vec!["вероятность", "шанс", "chance"]),
+            (&RandomChoose, vec!["выбери", "выбор", "choose"]),
+            (&GenerateNonsense, vec!["бред", "давай", "nonsense"]),
+            (&Morph, vec!["морф", "морфируй", "morph"]),
             (
                 &MorphDebug,
-                vec![
-                    vec![Word("морф"), Word("дебаг")],
-                    vec![Word("морфируй"), Word("дебаг")],
-                    vec![Word("morph"), Word("debug")],
-                ],
+                vec!["морф дебаг", "морфируй дебаг", "morph debug"],
             ),
-            (
-                &Quote,
-                vec![vec![Word("цит")], vec![Word("цитата")], vec![Word("quote")]],
-            ),
-            (
-                &Joke,
-                vec![
-                    vec![Word("анекдот")],
-                    vec![Word("анек")],
-                    vec![Word("joke")],
-                ],
-            ),
-            (&Advice, vec![vec![Word("совет")], vec![Word("advice")]]),
-        ]);
-        assert!(
-            Command::iter().all(|x| result.contains_key(&x)),
-            "Command not contains keywords"
-        );
+            (&Quote, vec!["цит", "цитата", "quote"]),
+            (&Joke, vec!["анекдот", "анек", "joke"]),
+            (&Advice, vec!["совет", "advice"]),
+        ];
+        let property_commands = property
+            .iter()
+            .map(|(command, _)| *command)
+            .collect::<HashSet<_>>();
+        assert!(Command::iter().all(|x| property_commands.contains(&x)));
+        let mut result: Vec<(&Command, Vec<Token>)> = property
+            .iter()
+            .flat_map(|(command, vec_keywords)| {
+                vec_keywords
+                    .iter()
+                    .map(|keyword| (*command, tokenize(keyword)))
+            })
+            .collect();
+        // Sorting keywords token by len of tokens desc for funnel search
+        result.sort_by_key(|(_, c)| Reverse(c.len()));
         result
     })
 }
@@ -168,38 +99,34 @@ fn bot_key_word<'a>(value: &'a Vec<Token>) -> Option<Token<'a>> {
         }
     })
 }
-
-pub fn to_command(tokens: Vec<Token>) -> Option<CommandProperty> {
+pub fn to_command_property(tokens: Vec<Token>) -> Option<CommandProperty> {
     bot_key_word(&tokens)?;
-    command_keywords()
-        .iter()
-        .find_map(|(command_key, command_collection)| {
-            command_collection.iter().find_map(|keyword_tokens| {
-                if keyword_tokens.iter().enumerate().all(|(i, t)| {
-                    keyword_tokens.len() < tokens.len()
-                        && tokens.len() > i + 1
-                        && &tokens[i + 1] == t
-                }) {
-                    Some(CommandProperty {
-                        command: command_key,
-                        command_end_position: keyword_tokens.len(),
-                    })
-                } else {
-                    None
-                }
+    command_keywords().iter().find_map(|(command, keywords)| {
+        if keywords.iter().enumerate().all(|(i, t)| {
+            keywords.len() < tokens.len() && tokens.len() > i + 1 && &tokens[i + 1] == t
+        }) {
+            Some(CommandProperty {
+                command,
+                command_end_position: keywords.len(),
             })
-        })
+        } else {
+            None
+        }
+    })
 }
 
 #[cfg(test)]
 mod tests {
     use Command::*;
 
-    use crate::core::command::{bot_key_word, to_command, Command, CommandProperty};
+    use crate::core::command::{
+        bot_key_word, command_keywords, to_command_property, Command, CommandProperty,
+    };
     use crate::core::lexer::{tokenize, Token};
 
     #[test]
     fn test_bot_key_word() {
+        command_keywords();
         for (input, output) in [
             ("Хлеб кто булочка?", Some(Token::Word("Хлеб"))),
             ("хлеб кто булочка?", Some(Token::Word("хлеб"))),
@@ -270,7 +197,7 @@ mod tests {
             // Without caption
             (None, None),
         ] {
-            assert_eq!(to_command(tokenize(input.unwrap_or_default())), output);
+            assert_eq!(to_command_property(tokenize(input.unwrap_or_default())), output);
         }
     }
 }
