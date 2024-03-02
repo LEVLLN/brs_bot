fe4bf34970120de84740b3834e7f55f1552d6672
Lev <lev.kudryashov1994@gmail.com>
2024-03-02 20:13:23 +0500

    Migrate answer_chance from answer_packs table to chats table

---
+++ migrations/20240302151126_substring_answer_change_migrate_data.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+UPDATE chats AS ch
+SET substring_answer_chance = answer_packs.answer_chance
+FROM answer_packs, answer_packs_to_chats
+WHERE ch.id = answer_packs_to_chats.chat_id AND answer_packs.id = answer_packs_to_chats.pack_id AND answer_packs.name = 'default';
